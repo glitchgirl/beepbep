7f4dd4ad97fe91e7db80289a73db109213c25092
glitchgirl <londonspleen@gmail.com>
2020-03-20 11:16:38 -0400

    annoying

---
+++ functions.js
@@ -0,0 +1,6 @@
+// this is where the exports come from i.e. code we want to be able to reuse/ship
+// we want to print stuff
+
+export function print(){
+    console.log("wonderful");
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+// this is where we are going to run our code from, and this is where we import into
+
+import {print} from './functions.js';
+
+print(); 
